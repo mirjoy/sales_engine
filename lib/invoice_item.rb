9b6db17204006129cff0b770983300c338d876e8
Miriam Moser <mirjoy.moser@gmail.com>
2015-01-20 16:19:13 -0700

    invoice item class created

--- lib/invoice_item.rb
+++ lib/invoice_item.rb
@@ -1,43 +1,20 @@
-require 'CSV'
-require 'pry'
-
 class InvoiceItem
-  attr_reader :list_of_invoice_items
+  attr_reader :id,
+              :item_id,
+              :invoice_id,
+              :quantity,
+              :unit_price,
+              :created_at,
+              :updated_at
 
-  def initialize
+  def initialize(row)
+    @id = row[:id]
+    @item_id = row[:item_id]
+    @invoice_id = row[:invoice_id]
+    @quantity = row[:quantity]
+    @unit_price = row[:unit_price]
+    @created_at = row[:created_at]
+    @updated_at = row[:updated_at]
   end
-  #
-  # def id
-  #   # list_of_invoice_items.rewind
-  #
-  #   list_of_invoice_items.each do |row|
-  #     row[:id]
-  #     binding.pry
-  #   end
-  # end
-  #
-  # def item_id
-  #
-  # end
-  #
-  # def invoice_id
-  #
-  # end
-  #
-  # def quantity
-  #
-  # end
-  #
-  # def unit_price
-  #
-  # end
-  #
-  # def created_at
-  #
-  # end
-  #
-  # def updated_at
-  #
-  # end
 
 end
--- test/invoice_item_test.rb
+++ test/invoice_item_test.rb
@@ -1,20 +1,88 @@
+require 'CSV'
+require './lib/invoice_item'
 require 'minitest/autorun'
 require 'minitest/pride'
-require './lib/invoice_item'
 
 class InvoiceItemTest < Minitest::Test
-  attr_reader :invoice_item
-
-  def setup
-    @invoice_item = InvoiceItem.new
-  end
 
   def test_it_exists
     assert InvoiceItem
   end
 
-  def test_we_can_find_id
-    skip
+  def test_find_id
+    invoice_items = CSV.open('./data/invoice_items.csv', headers: true, header_converters: :symbol)
+
+    one_invoice_item = invoice_items.each do |ii|
+      return ii
+    end
+
+    invoice_items = InvoiceItem.new(one_invoice_item)
+    assert_equal 1, invoice_items.id
   end
 
+  def test_find_item_id
+    invoice_items = CSV.open('./data/invoice_items.csv', headers: true, header_converters: :symbol)
+
+    one_invoice_item = invoice_items.each do |ii|
+      return ii
+    end
+
+    invoice_items = InvoiceItem.new(one_invoice_item)
+    assert_equal 539, invoice_items.item_id
+  end
+
+  def test_find_invoice_id
+    invoice_items = CSV.open('./data/invoice_items.csv', headers: true, header_converters: :symbol)
+
+    one_invoice_item = invoice_items.each do |ii|
+      return ii
+    end
+
+    invoice_items = InvoiceItem.new(one_invoice_item)
+    assert_equal 1, invoice_items.invoice_id
+  end
+
+  def test_find_quantity
+    invoice_items = CSV.open('./data/invoice_items.csv', headers: true, header_converters: :symbol)
+
+    one_invoice_item = invoice_items.each do |ii|
+      return ii
+    end
+
+    invoice_items = InvoiceItem.new(one_invoice_item)
+    assert_equal 5, invoice_items.quantity
+  end
+
+  def test_find_unit_price
+    invoice_items = CSV.open('./data/invoice_items.csv', headers: true, header_converters: :symbol)
+
+    one_invoice_item = invoice_items.each do |ii|
+      return ii
+    end
+
+    invoice_items = InvoiceItem.new(one_invoice_item)
+    assert_equal 13635, invoice_items.unit_price
+  end
+
+  def test_find_created_at
+    invoice_items = CSV.open('./data/invoice_items.csv', headers: true, header_converters: :symbol)
+
+    one_invoice_item = invoice_items.each do |ii|
+      return ii
+    end
+
+    invoice_items = InvoiceItem.new(one_invoice_item)
+    assert_equal "2012-03-27 14:54:09 UTC", invoice_items.created_at
+  end
+
+  def test_find_updated_at
+    invoice_items = CSV.open('./data/invoice_items.csv', headers: true, header_converters: :symbol)
+
+    one_invoice_item = invoice_items.each do |ii|
+      return ii
+    end
+
+    invoice_items = InvoiceItem.new(one_invoice_item)
+    assert_equal "2012-03-27 14:54:09 UTC", invoice_items.updated_at
+  end
 end
