41abb62b42fe5e11703751f147b0c22005233572
Miriam Moser <mirjoy.moser@gmail.com>
2015-01-21 07:21:57 -0700

    added invoice and transaction classes

---
+++ test/invoice_test.rb
@@ -0,0 +1,79 @@
+require 'CSV'
+require './lib/invoice'
+require 'minitest/autorun'
+require 'minitest/pride'
+require 'pry'
+
+class InvoiceTest < Minitest::Test
+
+  def test_it_exists
+    assert Invoice
+  end
+
+  def test_find_id
+    invoice = CSV.open('./data/invoices.csv', headers: true, header_converters: :symbol)
+
+    one_invoice = invoice.each do |t|
+      return t
+    end
+
+    invoice_one = Invoice.new(one_invoice)
+    assert_equal 1, invoice_one.id
+  end
+
+  def test_find_customer_id
+    invoice = CSV.open('./data/invoices.csv', headers: true, header_converters: :symbol)
+
+    one_invoice = invoice.each do |t|
+      return t
+    end
+
+    invoice_one = Invoice.new(one_invoice)
+    assert_equal 1, invoice_one.customer_id
+  end
+
+  def test_find_merchant_id
+    invoice = CSV.open('./data/invoices.csv', headers: true, header_converters: :symbol)
+
+    one_invoice = invoice.each do |t|
+      return t
+    end
+
+    invoice_one = Invoice.new(one_invoice)
+    assert_equal 26, invoice_one.merchant_id
+  end
+
+  def test_find_status
+    invoice = CSV.open('./data/invoices.csv', headers: true, header_converters: :symbol)
+
+    one_invoice = invoice.each do |t|
+      return t
+    end
+
+    invoice_one = Invoice.new(one_invoice)
+    assert_equal "shipped", invoice_one.status
+  end
+
+  def test_find_created_at
+    invoice = CSV.open('./data/invoices.csv', headers: true, header_converters: :symbol)
+
+    one_invoice = invoice.each do |t|
+      return t
+    end
+
+    invoice_one = Invoice.new(one_invoice)
+    assert_equal "2012-03-25 09:54:09 UTC", invoice_one.created_at
+  end
+
+  def test_find_updated_at
+    invoice = CSV.open('./data/invoices.csv', headers: true, header_converters: :symbol)
+
+    one_invoice = invoice.each do |t|
+      return t
+    end
+
+    invoice_one = Invoice.new(one_invoice)
+    assert_equal "2012-03-25 09:54:09 UTC", invoice_one.updated_at
+  end
+
+end
---
+++ test/transaction_test.rb
@@ -0,0 +1,89 @@
+require 'CSV'
+require './lib/transaction'
+require 'minitest/autorun'
+require 'minitest/pride'
+require 'pry'
+
+class TransactionTest < Minitest::Test
+
+  def test_it_exists
+    assert Transaction
+  end
+
+  def test_find_id
+    transact = CSV.open('./data/transactions.csv', headers: true, header_converters: :symbol)
+
+    one_transaction = transact.each do |t|
+      return t
+    end
+
+    transaction_one = Transaction.new(one_transaction)
+    assert_equal 1, transaction_one.id
+  end
+
+  def test_find_invoice_id
+    transact = CSV.open('./data/transactions.csv', headers: true, header_converters: :symbol)
+
+    one_transaction = transact.each do |t|
+      return t
+    end
+
+    transaction_one = Transaction.new(one_transaction)
+    assert_equal 1, transaction_one.invoice_id
+  end
+
+  def test_find_credit_card_number
+    transact = CSV.open('./data/transactions.csv', headers: true, header_converters: :symbol)
+
+    one_transaction = transact.each do |t|
+      return t
+    end
+
+    transaction_one = Transaction.new(one_transaction)
+    assert_equal "4654405418249632", transaction_one.credit_card_number
+  end
+
+  def test_find_credit_card_expiration_date
+    transact = CSV.open('./data/transactions.csv', headers: true, header_converters: :symbol)
+
+    one_transaction = transact.each do |t|
+      return t
+    end
+
+    transaction_one = Transaction.new(one_transaction)
+    assert_equal " ", transaction_one.credit_card_expiration_date
+  end
+
+  def test_find_result
+    transact = CSV.open('./data/transactions.csv', headers: true, header_converters: :symbol)
+
+    one_transaction = transact.each do |t|
+      return t
+    end
+
+    transaction_one = Transaction.new(one_transaction)
+    assert_equal "success", transaction_one.result
+  end
+
+  def test_find_created_at
+    transact = CSV.open('./data/transactions.csv', headers: true, header_converters: :symbol)
+
+    one_transaction = transact.each do |t|
+      return t
+    end
+
+    transaction_one = Transaction.new(one_transaction)
+    assert_equal "2012-03-27 14:54:09 UTC", transaction_one.created_at
+  end
+
+  def test_find_updated_at
+    transact = CSV.open('./data/transactions.csv', headers: true, header_converters: :symbol)
+
+    one_transaction = transact.each do |t|
+      return t
+    end
+
+    transaction_one = Transaction.new(one_transaction)
+    assert_equal "2012-03-27 14:54:09 UTC", transaction_one.updated_at
+  end
+end
