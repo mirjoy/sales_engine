eb0b3ccf84889caa7c09d9d8678f5a82330671a6
Miriam Moser <mirjoy.moser@gmail.com>
2015-01-22 07:20:11 -0700

    added invoice item repo and test

--- lib/invoice_item.rb
+++ lib/invoice_item.rb
@@ -1,20 +1,22 @@
 class InvoiceItem
-  attr_reader :id,
-              :item_id,
-              :invoice_id,
-              :quantity,
-              :unit_price,
-              :created_at,
-              :updated_at
+    attr_reader :id,
+                :item_id,
+                :invoice_id,
+                :quantity,
+                :unit_price,
+                :created_at,
+                :updated_at
+  attr_accessor :parent_class
 
-  def initialize(row)
-    @id = row[:id]
-    @item_id = row[:item_id]
-    @invoice_id = row[:invoice_id]
-    @quantity = row[:quantity]
-    @unit_price = row[:unit_price]
+  def initialize(row, parent_class)
+    @id = row[:id].to_i
+    @item_id = row[:item_id].to_i
+    @invoice_id = row[:invoice_id].to_i
+    @quantity = row[:quantity].to_i
+    @unit_price = row[:unit_price].to_i
     @created_at = row[:created_at]
     @updated_at = row[:updated_at]
+    @parent_class = parent_class
   end
 
 end
--- test/invoice_item_repo_test.rb
+++ test/invoice_item_repo_test.rb
@@ -1,7 +1,60 @@
+require 'csv'
+require 'minitest/autorun'
+require 'minitest/pride'
+require_relative '../lib/invoice_item_repo'
 
-class InvoiceItemRepo
+class InvoiceItemRepoTest < Minitest::Test
+  attr_accessor :invoice_item_repo
 
-  # @invoice_item = InvoiceItem.new
+  def setup
+    file = "./test/support/sample_invoice_items.csv"
+    @invoice_item_repo = InvoiceItemRepo.new(file, nil)
+  end
 
+  def test_it_exists
+    assert InvoiceItemRepo
+  end
+
+  def test_it_finds_all_invoice_items
+    assert_equal 5, invoice_item_repo.all.count
+  end
+
+  def test_it_finds_a_random_invoice_item
+    assert invoice_item_repo.random
+  end
+
+  def test_it_finds_the_third_invoice_item
+    assert_equal 523, invoice_item_repo.all[2].item_id
+  end
+
+  def test_it_finds_all_invoice_items_by_invoice_id
+    inv = invoice_item_repo.find_all_by_invoice_id(1)
+    assert_equal 5, inv.count
+  end
+
+  def test_it_finds_an_invoice_item_by_quantity
+    inv = invoice_item_repo.find_by_quantity(9)
+    assert_equal 528, inv.item_id
+  end
+
+  def test_it_finds_all_invoice_items_by_unit_price
+    inv = invoice_item_repo.find_by_unit_price(2196)
+    assert_equal 3, inv.quantity
+  end
+
+  def test_it_finds_a_transaction_by_created_at
+    inv = invoice_item_repo.find_all_by_created_at("2012-03-27 14:54:09 UTC")
+    assert_equal 5, inv.count
+  end
+
+  def test_it_finds_no_invoice_item_by_created_at
+    inv = invoice_item_repo.find_all_by_created_at("2012-03-10 00:54:19 UTC")
+    assert_equal 0, inv.count
+  end
+
+  def test_it_finds_invoice_item_by_updated_at
+    inv = invoice_item_repo.find_all_by_updated_at("2012-03-27 14:54:09 UTC")
+    assert_equal 5, inv.count
+  end
 
 end
